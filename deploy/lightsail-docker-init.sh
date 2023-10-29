b9796a6a8f82d9e24f4b871f8d54052f6aff5936
meisimo <siruasor@gmail.com>
2023-10-29 18:01:31 -0500

    Creating a script to run to deploy in lightsail and a startup setting

---
+++ deploy/lightsail-docker-init.sh
@@ -0,0 +1,30 @@
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl gnupg
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo docker run hello-world
+
+# Installling docker compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+sudo apt install git
+mkdir app
+cd app
+git clone https://github.com/meisimo/poke-api.git
+cd poke-api
+
+# Start the app on os startup
+sudo cp scripts/docker-compose-app.service /etc/systemd/system/docker-compose-app.service
+systemctl enable docker-compose-app
+
